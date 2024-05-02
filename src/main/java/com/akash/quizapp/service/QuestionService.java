0d534b7d1c737695b3db14513c2a62aa795a3865
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-05-03 02:24:28 +0600

    first

---
+++ src/main/java/com/akash/quizapp/service/QuestionService.java
@@ -0,0 +1,23 @@
+package com.akash.quizapp.service;
+
+import com.akash.quizapp.Question;
+import com.akash.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
+
+    public List<Question> getQuestionByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
+}
